460d63cf078f567640da67ec6e0f8efd55178fdd
sukhdeep16 <48015933+sukhdeep16@users.noreply.github.com>
2024-10-04 11:47:04 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "5. please visit my website learning-ocean.com -- sukhdeep singh --- 2nd changes done --- now this is git automated";
+        return "5. please visit my website learning-ocean.com -- sukhdeep singh --- 2nd changes done --- now this is git automated --one";
     }
 
 }
